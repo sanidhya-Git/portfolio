9d6c7343ba89db7d42dd56001fcabece0d839fbc
sanidhya-Git <sanidhyamehra36@gmail.com>
2025-03-13 21:03:22 +0530

    theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,18 @@
-import type React from "react"
-import type { Metadata } from "next"
-import { Inter } from "next/font/google"
-import "./globals.css"
-import { ThemeProvider } from "@/components/themeProvider"
-import Header from "@/components/header"
-import Footer from "@/components/footer"
-import { Toaster} from "react-hot-toast"
+import type React from "react";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { ThemeProvider } from "@/components/themeProvider";
+import Header from "@/components/header";
+import Footer from "@/components/footer";
+import { Toaster } from "react-hot-toast";
 
-const inter = Inter({ subsets: ["latin"] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Sanidhya Mehra | Full Stack Developer",
   description: "Portfolio website showcasing my projects and skills as a full stack developer",
-}
+};
 
 export default function RootLayout({
   children,
@@ -20,23 +20,20 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    
-      <html lang="en" suppressHydrationWarning>
-        <body>
-          <ThemeProvider
-            attribute="class"
-            defaultTheme="dark"
-            enableSystem
-            disableTransitionOnChange
-          >
-            <Header />
-            <Toaster position="top-center" reverseOrder={false} />
-            {children}
-            <Footer />
-          </ThemeProvider>
-        </body>
-      </html>
-   
+    <html lang="en" suppressHydrationWarning>
+      <body className={inter.className}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <Header />
+          <Toaster position="top-center" reverseOrder={false} />
+          {children}
+          <Footer />
+        </ThemeProvider>
+      </body>
+    </html>
   );
 }
-
