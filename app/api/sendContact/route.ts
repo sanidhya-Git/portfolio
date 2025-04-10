67e5a02e7316ebd16c1cef8bbf40fc88ff822f31
sanidhya-Git <sanidhyamehra36@gmail.com>
2025-04-11 01:06:59 +0530

    production

--- app/api/sendContact/route.ts
+++ app/api/sendContact/route.ts
@@ -1,17 +1,21 @@
 import { NextResponse } from "next/server";
 import nodemailer from "nodemailer";
+// import { env } from "~/env";
 
 export async function POST(req: Request) {
   const { name, email, subject, message } = await req.json();
 
 
-  const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
-    },
-  });
+ const transporter = nodemailer.createTransport({
+  service: "gmail",
+  host: process.env.EMAIL_HOST,
+  port: parseInt(process.env.EMAIL_PORT!),
+  secure: true,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
 
   const mailOptions = {
     from: process.env.EMAIL_USER,
